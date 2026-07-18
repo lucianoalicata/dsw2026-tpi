6b8bb683d4760e2303ae8a00c1b1b62a5d9f024b
Luciano Alicata <LucianoAntonioAlicata@alu.frt.utn.edu.ar>
2026-07-17 23:46:14 -0300

    agrego Patient a las entidades del dominio

--- Dsw2026Tpi.Domain/Entities/Patient.cs
+++ Dsw2026Tpi.Domain/Entities/Patient.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace Dsw2026Tpi.Domain.Entities
 {
-    internal class Patient
+    public class Patient:EntityBase
     {
+        public Guid UserId { get; init; }
+        public string Dni { get; init; }
+        public string FullName { get; init; }
+        public string? Phone { get; set; }
+
+        #region Constructor for EF
+        #pragma warning disable CS8618
+        private Patient() {}
+        #pragma warning restore CS8618
+        #endregion
+
+        public Patient(Guid userId, string dni, string fullName, Guid? id = null) : base(id)
+        {
+            UserId = userId;
+            Dni = dni;
+            FullName = fullName;
+        }
     }
 }
