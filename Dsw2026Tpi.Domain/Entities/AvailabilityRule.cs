ab07e849f845223f92c1ab1ca1bad6d1a11a5b35
Luciano Alicata <LucianoAntonioAlicata@alu.frt.utn.edu.ar>
2026-07-17 23:46:30 -0300

    agrego regla de dsiponibilidad a las entidades del dominio

--- Dsw2026Tpi.Domain/Entities/AvailabilityRule.cs
+++ Dsw2026Tpi.Domain/Entities/AvailabilityRule.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace Dsw2026Tpi.Domain.Entities
 {
-    internal class AvailabilityRule
+    public class AvailabilityRule:EntityBase
     {
+        public Guid DoctorId { get; init; }
+        public Doctor? Doctor { get; private set; }
+        public int Month { get; init; }
+        public int Year { get; init; }
+        public int DayOfWeek { get; init; }
+        public TimeOnly StartTime { get; init; }
+        public TimeOnly EndTime { get; init; }
+
+        #region Constructor for EF
+        #pragma warning disable CS8618
+        private AvailabilityRule(){}
+        #pragma warning restore CS8618
+        #endregion
+
+        public AvailabilityRule(Guid doctorId, int month, int year, int dayOfWeek,
+            TimeOnly startTime, TimeOnly endTime, Guid? id = null) : base(id)
+        {
+            DoctorId = doctorId;
+            Month = month;
+            Year = year;
+            DayOfWeek = dayOfWeek;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
     }
 }
