a8c63f9c6a38174678216d3b4dcea168dac2fc0a
Juanita <JuanitaSofiaRivero@alu.frt.utn.edu.ar>
2026-07-21 18:02:41 -0300

    Clases AvailabilityRuleConfiguration y AvailabilitySlotConfiguration agregadas

---
+++ Dsw2026Tpi.Data/Configurations/AvailabilitySlotConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dsw2026Tpi.Data.Configurations;
+
+public class AvailabilitySlotConfiguration : IEntityTypeConfiguration<AvailabilitySlot>
+{
+    public void Configure(EntityTypeBuilder<AvailabilitySlot> builder)
+    {
+        builder.ToTable("AvailabilitySlots");
+        builder.HasIndex(s => new { s.DoctorId, s.SlotDate, s.StartTime }).IsUnique();
+
+        // Guarda el enum como texto (varchar) en lugar de nª
+        builder.Property(s => s.Status).HasConversion<string>().HasMaxLength(20);
+    }
+}
