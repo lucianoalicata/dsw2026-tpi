e8d967761056b20a4b1db3df058d9e90b0050f15
Luciano Alicata <LucianoAntonioAlicata@alu.frt.utn.edu.ar>
2026-07-21 16:16:23 -0300

    Dto AppointmentModel en capa Application

--- Dsw2026Tpi.Application/Dtos/AppointmentModel.cs
+++ Dsw2026Tpi.Application/Dtos/AppointmentModel.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Dsw2026Tpi.Application.Dtos;
 
-namespace Dsw2026Tpi.Application.Dtos
+public record AppointmentModel
 {
-    internal class AppointmentModel
-    {
-    }
+    public record Request(Guid DoctorId, Guid AvailabilityId, PatientDto Patient, string Reason);
+    public record PatientDto(string Dni);
+    public record Response(Guid Id, string DoctorName, string Speciality, DateOnly Date, TimeOnly Time, string Status);
 }
