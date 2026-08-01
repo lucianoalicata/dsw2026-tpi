2c9bec2fbc8fbdd78de5d986516742b74ba994e2
Luciano Alicata <LucianoAntonioAlicata@alu.frt.utn.edu.ar>
2026-08-01 15:55:14 -0300

    creación de proyecto xUnit en la solución

---
+++ Dsw2026Tpi.Test/AppointmentServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Dsw2026Tpi.Application.Services;
+using Dsw2026Tpi.CrossCutting.Exceptions;
+using Dsw2026Tpi.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using static Dsw2026Tpi.Application.Dtos.AppointmentModel;
+
+namespace Dsw2026Tpi.Test
+{
+    public class AppointmentServiceTest
+    {
+        private readonly IPersistence _mockPersistence=Substitute.For<IPersistence>();
+        private readonly ILogger<AppointmentService> _loggerMock = Substitute.For<ILogger<AppointmentService>>();
+        private readonly AppointmentService _service;
+
+        public AppointmentServiceTest()
+        {
+            _service = new AppointmentService(_mockPersistence, _loggerMock);
+        }
+
+        [Fact]
+        public async Task BookAppointment_CuandoElDniEsInvalidoPorNoTenerLongitudEntre7Y10Caracteres_EntoncesLanzaUnaValidationException()
+        {
+            //arrange
+            var request = new Request(
+                Guid.NewGuid(), 
+                Guid.NewGuid(), 
+                new PatientDto("456"), 
+                "consulta anual");
+
+            //act y assert
+            await Assert.ThrowsAsync<ValidationException>(() => _service.BookAppointment(request));
+        }
+    }
+}
