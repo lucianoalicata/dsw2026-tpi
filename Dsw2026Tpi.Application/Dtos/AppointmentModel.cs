58be73ed82d76596b60c824c4059db49733a93b4
Luciano Alicata <LucianoAntonioAlicata@alu.frt.utn.edu.ar>
2026-08-02 12:52:02 -0300

    se cambia el DTO de Appointment para cumplir con la consigna

--- Dsw2026Tpi.Application/Dtos/AppointmentModel.cs
+++ Dsw2026Tpi.Application/Dtos/AppointmentModel.cs
@@ -2,7 +2,7 @@
 
 public record AppointmentModel
 {
-    public record Request(Guid DoctorId, Guid AvailabilityId, PatientDto Patient, string Reason);
+    public record Request(Guid DoctorId, Guid AvailabilitySlotId, PatientDto Patient, string Reason);
     public record PatientDto(string Dni);
     public record Response(Guid Id, string DoctorName, string Speciality, DateOnly Date, TimeOnly Time, string Status);
 }
